28807ab202c60adeac9d1a7d5d5b962241186de5
Martin Antonio <mancorge@gmail.com>
2023-02-14 00:05:07 +0100

    Botones de imagenes en pantalla de inicio añadidos

--- lib/main.dart
+++ lib/main.dart
@@ -40,35 +40,54 @@ class Presentation extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              ElevatedButton(
-                child: const Text('Xylophone'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Xylophone()),
-                  );
-                },
+              CircleAvatar(
+                radius: 80,
+                backgroundColor: Color.fromRGBO(210, 100, 180, 1),
+                child: TextButton(
+                  child: const Image(
+                    image: AssetImage("images/xylophone.png"),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Xylophone()),
+                    );
+                  },
+                ),
               ),
-              ElevatedButton(
-                child: const Text('Guitar'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Guitar()),
-                  );
-                },
+              CircleAvatar(
+                radius: 80,
+                backgroundColor: Colors.orange,
+                child: TextButton(
+                  child: const Image(
+                    image: AssetImage("images/guitar.png"),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Guitar()),
+                    );
+                  },
+                ),
               ),
-              ElevatedButton(
-                child: const Text('Bongos'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Bongos()),
-                  );
-                },
+              CircleAvatar(
+                radius: 80,
+                backgroundColor: Colors.lightBlueAccent,
+                child: TextButton(
+                  child: const Image(
+                    image: AssetImage("images/bongos.png"),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Bongos()),
+                    );
+                  },
+                ),
               ),
             ],
           ),
